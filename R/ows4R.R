d90cc257e6d4a0f6dddd85d2ac3ac9ac17cfecc0
eblondel <emmanuel.blondel1@gmail.com>
2018-02-14 21:36:24 +0100

    #1 init package

---
+++ R/ows4R.R
@@ -0,0 +1,25 @@
+#' @name ows4R
+#' @aliases ows4R-package
+#' @aliases ows4R
+#' @docType package
+#' 
+#' @title Interface to OGC Web-Services
+#' @description Provides an interface to OGC Web-Services (OWS). In a first step, the package supports the Common
+#' OGC Web-Services specifications the Web Feature Service (WFS). ows4R will progressively support other OGC web
+#' service specifications such as Web Map Service (WMS), Web Coverage Service, or Catalogue Service for the web (CSW).
+#'
+#' 
+#' @details
+#'  \tabular{ll}{
+#'    Package: \tab ows4R\cr
+#'    Type: \tab Package\cr
+#'    Version
+#'    : \tab 0.1\cr
+#'    Date: \tab 2018-02-14\cr
+#'    License: \tab GPL(>=2.0)\cr
+#'    LazyLoad: \tab yes\cr
+#'  }
+#'  
+#'@author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
+#'
+NULL
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ R client for OGC Web-Services
 * the Common OGC Web-Services specifications
 * the Web Feature Service (WFS), with support for all versions (_i.e._ ``1.0.0``, ``1.1.0``, and ``2.0.0``).
 
-In addition, a native R WMS client is going to be investigated.
 
-For more information, or if you are interested in funding this R package project, do not hesitate to contact me.
+
+For more information, or if you are interested in funding this R package project or to contribute to it, do not hesitate to contact me by [e-mail](mailto:emmanuel.blondel1@gmail.com)
+
