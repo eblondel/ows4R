84a3a93c9e1d92d60f490aec6555e6ac4542d569
eblondel <emmanuel.blondel1@gmail.com>
2018-02-16 21:29:29 +0100

    #3 fix docker geonetwork

--- tests/testthat/test_CSWClient.R
+++ tests/testthat/test_CSWClient.R
@@ -1,14 +1,14 @@
-# test_WFS.R
+# test_CSW.R
 # Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
 #
-# Description: Integration tests for WFS Client
+# Description: Integration tests for CSW Client
 #=======================
 require(ows4R, quietly = TRUE)
 require(testthat)
 context("WFS")
 
 test_that("CSW 2.0.2",{
-  csw <- CSWClient$new("http://localhost:8080/geonetwork/srv/eng/csw", "2.0.2")
+  csw <- CSWClient$new("http://localhost:8282/geonetwork/srv/eng/csw", "2.0.2")
   expect_is(csw, "CSWClient")
   caps <- csw$getCapabilities()
   expect_is(caps, "CSWCapabilities")
