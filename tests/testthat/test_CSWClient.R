e285b0beea03c30fb2b7a027a974af228bbe32a6
eblondel <emmanuel.blondel1@gmail.com>
2018-04-14 23:26:15 +0200

    csw tests

--- tests/testthat/test_CSWClient.R
+++ tests/testthat/test_CSWClient.R
@@ -8,6 +8,8 @@ require(geometa)
 require(testthat)
 context("CSW")
 
+#CSW 2.0.2 – GetCapabilities
+
 test_that("CSW 2.0.2 - GetCapabilities | GeoNetwork",{
   csw <- CSWClient$new("http://localhost:8282/geonetwork/srv/eng/csw", "2.0.2", logger = "INFO")
   expect_is(csw, "CSWClient")
@@ -22,10 +24,14 @@ test_that("CSW 2.0.2 - GetCapabilities | pycsw",{
   expect_is(caps, "CSWCapabilities")
 })
 
+#CSW 2.0.2 – DescribeRecord
+
 #test_that("CSW 2.0.2 - DescribeRecord",{
 #  csw <- CSWClient$new("http://localhost:8282/geonetwork/srv/en/csw", "2.0.2", logger = "INFO")
 #  xsd <- csw$describeRecord(outputSchema = "http://www.isotc211.org/2005/gmd")
 #})
+
+#CSW 2.0.2 – GetRecordById
 
 test_that("CSW 2.0.2 - GetRecordById",{
   csw <- CSWClient$new("http://www.fao.org/geonetwork/srv/en/csw", "2.0.2", logger = "INFO")
@@ -33,10 +39,11 @@ test_that("CSW 2.0.2 - GetRecordById",{
   expect_is(md, "ISOMetadata")
 })
 
-#test_that("CSW 2.0.2 - GetRecords",{
-#  csw <- CSWClient$new("http://www.fao.org/geonetwork/srv/en/csw", "2.0.2", logger = "INFO")
-#  mdlist <- csw$getRecords(constraint = "AnyText+like+%cwp-grid%", outputSchema = "http://www.isotc211.org/2005/gmd")
-#  expect_is(mdlist, "list")
-#  expect_equal(unique(sapply(mdlist, function(x) {class(x)[1]})), "ISOMetadata")
-#})
+#CSW 2.0.2 – GetRecords
+
+test_that("CSW 2.0.2 - GetRecords",{
+  csw <- CSWClient$new("http://www.fao.org/geonetwork/srv/en/csw", "2.0.2", logger = "INFO")
+  mdlist <- csw$getRecords(constraint = "AnyText+like+%cwp-grid%", outputSchema = "http://www.isotc211.org/2005/gmd")
+  expect_equal(unique(sapply(mdlist, is)), "ISOMetadata")
+})
 
