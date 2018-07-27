5ec8ef6d3053d67badee3e9a06d6c20821191620
eblondel <emmanuel.blondel1@gmail.com>
2018-07-27 22:07:46 +0200

    #2 split WFSClient tests by version

---
+++ tests/testthat/test_WFSClient_v1_0_0.R
@@ -0,0 +1,22 @@
+# test_WFSClient_v1_0_0.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Integration tests for WFS Client version 1.0.0
+#=======================
+require(ows4R, quietly = TRUE)
+require(testthat)
+context("WFS")
+
+test_that("WFS 1.0.0",{
+  wfs <- WFSClient$new("http://localhost:8080/geoserver/wfs", "1.0.0", logger = "INFO")
+  expect_is(wfs, "WFSClient")
+  caps <- wfs$getCapabilities()
+  expect_is(caps, "WFSCapabilities")
+  ft <- caps$findFeatureTypeByName("topp:tasmania_water_bodies", exact = TRUE)
+  expect_is(ft, "WFSFeatureType")
+  ft.des <- ft$getDescription()
+  expect_is(ft.des, "list")
+  ft.sp <- ft$getFeatures()
+  expect_is(ft.sp, "sf")
+  expect_is(ft.sp, "data.frame")
+})
