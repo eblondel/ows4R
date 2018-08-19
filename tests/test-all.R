c0420e9f3eab420135cfdc422935794575c2d68d
eblondel <emmanuel.blondel1@gmail.com>
2018-08-19 19:30:05 +0200

    fix for CRAN submission

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,2 +1,10 @@
 library(testthat)
-test_check("ows4R")
+library(httr)
+
+runIntegrationTests <- class(try(httr::GET("http://localhost:8000/csw"))) != "try-error"
+if(runIntegrationTests){
+  cat(sprintf("Running integration tests...\n"))
+  test_check("ows4R")
+}else{
+  cat("Skipping integration tests...\n")
+}
