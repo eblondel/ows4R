fd9c0b37257f5047ff8cd3a7aeb50cbe34a98c70
eblondel <emmanuel.blondel1@gmail.com>
2020-02-27 16:24:18 +0100

    update pkg date

--- R/ows4R.R
+++ R/ows4R.R
@@ -16,7 +16,7 @@
 #'    Type: \tab Package\cr
 #'    Version
 #'    : \tab 0.1-5\cr
-#'    Date: \tab 2020-02-05\cr
+#'    Date: \tab 2020-02-27\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
