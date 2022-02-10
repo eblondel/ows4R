b2a314717a7b17aa51d70b5da3680218e21db8cb
eblondel <emmanuel.blondel1@gmail.com>
2022-02-10 22:56:52 +0100

    #9 add WCS test 2.0.1 with Rasdaman

--- tests/testthat/test_WCSClient_v2_0.R
+++ tests/testthat/test_WCSClient_v2_0.R
@@ -17,3 +17,13 @@ test_that("WCS 2.0.1 - GeoServer",{
   expect_equal(length(caps$getCoverageSummaries()), 5L)
 })
 
+test_that("WCS 2.0.1 - Rasdaman",{
+  wcs <- WCSClient$new("http://ows.rasdaman.org/rasdaman/ows", "2.0.1", logger = "DEBUG")
+  expect_is(wcs, "WCSClient")
+  caps <- wcs$getCapabilities()
+  expect_is(caps, "WCSCapabilities")
+  cov <- caps$findCoverageSummaryById("AverageChloroColor")
+  expect_is(cov, "WCSCoverageSummary")
+  expect_equal(length(caps$getCoverageSummaries()), 35L)
+})
+
