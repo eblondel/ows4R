da9115081e4852026dd00022e60df3807b3d8dd1
eblondel <emmanuel.blondel1@gmail.com>
2023-04-12 19:17:35 +0200

    fix #106

--- R/profile.R
+++ R/profile.R
@@ -1,5 +1,11 @@
 .onLoad <- function (libname, pkgname) { # nocov start
   
+  #make sure sf relies on... "sf" (aka ISO/OGC Simple Features)! and not s2
+  if(sf::sf_use_s2()){
+    packageStartupMessage("Google's s2 geometry is not OGC compliant!")
+    sf::sf_use_s2(FALSE)
+  }
+  
   ISOMetadataNamespace$GML$uri <- "http://www.opengis.net/gml"
   
   assign(".ows4R", new.env(), envir= asNamespace(pkgname))
