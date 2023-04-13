981b8039de9a5bde999d0ff90b7df54f9e19ed07
eblondel <emmanuel.blondel1@gmail.com>
2023-04-13 16:32:16 +0200

    #106 use options instead of sf_use_s2 to get rid of package startup messages

--- R/profile.R
+++ R/profile.R
@@ -2,8 +2,7 @@
   
   #make sure sf relies on... "sf" (aka ISO/OGC Simple Features)! and not s2
   if(sf::sf_use_s2()){
-    packageStartupMessage("Google's s2 geometry is not OGC compliant!")
-    sf::sf_use_s2(FALSE)
+    options(sf_use_s2 = FALSE)
   }
   
   ISOMetadataNamespace$GML$uri <- "http://www.opengis.net/gml"
