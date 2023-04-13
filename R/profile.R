164b51811951ccc54f2ea359f5553d1ebcb3efef
eblondel <emmanuel.blondel1@gmail.com>
2023-04-13 16:34:05 +0200

    #106

--- R/profile.R
+++ R/profile.R
@@ -1,9 +1,7 @@
 .onLoad <- function (libname, pkgname) { # nocov start
   
   #make sure sf relies on... "sf" (aka ISO/OGC Simple Features)! and not s2
-  if(sf::sf_use_s2()){
-    options(sf_use_s2 = FALSE)
-  }
+  options(sf_use_s2 = FALSE)
   
   ISOMetadataNamespace$GML$uri <- "http://www.opengis.net/gml"
   
